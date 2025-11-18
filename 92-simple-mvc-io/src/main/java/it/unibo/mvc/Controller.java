c69621aac270a542691f03b9333dae18072e6e77
Anna <annapollini2005@gmail.com>
2025-11-18 19:00:00 +0100

    Completed the second part of the exercise

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -5,39 +5,62 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 
-
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public final class Controller {
 
-    private static final String DEFAULT_FILE = System.getProperty("user.home")
+    private static final String DEFAULT_FILE = "output.txt";
+    private static final String HOME_FOLDER = System.getProperty("user.home");
+
+    /* By default, the current file is "output.txt" inside the user home folder */
+    private File currentFile = new File(
+            HOME_FOLDER
             + System.getProperty("file.separator")
-            + "output.txt";
+            + DEFAULT_FILE
+        );
 
-    private File currentFile;
-    
-    /* Method for setting a File as current file */
-    public void setFile (final File inputFile) {
+    /**
+     * Method for setting a File as current file.
+     * 
+     * @param inputFile the file to set as current file
+     */
+    public void setFile(final File inputFile) {
         this.currentFile = inputFile;
     }
 
-    /* Method for getting the current File */
-    public File getFile () {
+    /**
+     * Method for getting the current File.
+     * 
+     * @return the current file 
+     */
+    public File getFile() {
         return this.currentFile;
     }
 
-    /* Method for getting the PATH of the current File */
-    public String getPath () {
+    /**
+     * Method for getting the PATH of the current File.
+     * 
+     * @return the path of the current file
+     */
+    public String getPath() {
         return this.currentFile.getAbsolutePath();
     }
 
-    /* Method that gets a `String` as input and saves its content on the current file */
-    public void writeTheString (final String s) throws IOException{
-        try (PrintStream ps = new PrintStream(DEFAULT_FILE, StandardCharsets.UTF_8)) {
+    /**
+     * Method for getting the PATH of the current File.
+     * 
+     * @param s the string we wuold like to write
+     * 
+     * @throws IOException if the writing fails
+     */
+    public void writeTheString(final String s) throws IOException {
+        try (PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
             ps.print(s);
         } catch (final IOException e) {
-            System.out.println("This function cause an IoException");
+            System.out.println(
+                "IOException, inable to write string [" + s + "] in the file"
+            );
             e.printStackTrace(); // NOPMD: allowed as this is just an exercise
         }
     }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,61 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "My first java graphical interface";
+    private static final int PROPORTION = 5;
 
+    private final JFrame frame = new JFrame(TITLE);
+    private final Controller control = new Controller();
+
+    /**
+     * Creates a SimpleGUI.
+     */
+    public SimpleGUI() {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        //Text area
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        // Button save
+        final JButton buttonSave = new JButton("Save");
+        buttonSave.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent event) {
+                try {
+                    // We want to save what's in the text area
+                    control.writeTheString(textArea.getText());
+                } catch (final IOException e) {
+                    JOptionPane.showMessageDialog(
+                        frame, // On the centre of the frame
+                        "There's a problem when it comes to save what you've done ",
+                        e.getMessage(),
+                        JOptionPane.ERROR_MESSAGE
+                        );
+                }
+            }
+        });
+        canvas.add(buttonSave, BorderLayout.SOUTH);
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application.
+     *
+     * @param args ignored
+     */
+    public static void main(final String... args) {
+       new SimpleGUI().display();
+    }
 }
